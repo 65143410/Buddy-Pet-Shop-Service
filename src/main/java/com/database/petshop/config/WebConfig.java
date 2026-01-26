cec01035e253b707be87b04f5cbb9b4ee34a44b6
(410)Nong'P <65143410@g.cmru.ac.th>
2026-01-26 17:47:48 +0700

    8

--- src/main/java/com/database/petshop/config/WebConfig.java
+++ src/main/java/com/database/petshop/config/WebConfig.java
@@ -1,7 +1,7 @@
 package com.database.petshop.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry; 
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,10 +16,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") 
-                .allowedOrigins("http://localhost:4200") 
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") 
-                .allowedHeaders("*") 
-                .allowCredentials(true); 
+        registry.addMapping("/**")
+                // .allowedOrigins("http://localhost:4200")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
