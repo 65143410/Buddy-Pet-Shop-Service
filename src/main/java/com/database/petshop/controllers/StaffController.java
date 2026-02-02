fa78d4bb90f6bd413ac181737a6b715e36790a6b
(410)Nong'P <65143410@g.cmru.ac.th>
2026-02-02 10:29:39 +0700

    13

--- src/main/java/com/database/petshop/controllers/StaffController.java
+++ src/main/java/com/database/petshop/controllers/StaffController.java
@@ -22,7 +22,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/staff")
-// @CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class StaffController {
 
     @Autowired
